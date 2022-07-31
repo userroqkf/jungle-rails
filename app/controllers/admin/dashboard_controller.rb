bbdaefeab05004c5af715fe51e5313a1b01b50ba
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-07-31 16:11:09 +0800

    about page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
+    @products = Product.count
+    @categories  = Category.count
   end
 end
