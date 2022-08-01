47cfacdb49f6bcc3d0c0443e849471f638bf9c8b
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-08-01 15:32:29 +0800

    setting up rspec for jungle project

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,14 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, :categories, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show]
+    resources :categories
   end
 
     # these routes are for showing users a login form, logging them in, and logging them out.
-    get '/login' => 'sessions#new'
-    post '/login' => 'sessions#create'
-    get '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # These routes will be for signup. The first renders a form in the browse, the second will 
   # receive the form and create a user in our database using the data given to us by the user.
